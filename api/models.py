a287a9afcb64a75c13523352e27e08b83608fa5b
ExtaSsS4106 <ExtaZzZ123456789@yandex.ru>
2026-09-21 10:47:28 +0300

    init docs

--- api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-import json
-
-# Create your models here.
-class profiles(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=50, default='user')
